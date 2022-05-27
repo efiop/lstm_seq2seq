3a30704e8af1b37b700d8872e62b38bbb21ef862
dberenbaum <dave@iterative.ai>
2022-05-27 20:52:40 +0000

    dvc: commit experiment 524c82629d2da48501345fe13512ba5ff2da5bab45aeccc4e660fcd0badb8430

--- params.py
+++ params.py
@@ -1,6 +1,6 @@
 batch_size = 64  # Batch size for training.
-epochs = 1  # Number of epochs to train for.
-latent_dim = 256  # Latent dimensionality of the encoding space.
+epochs = 10  # Number of epochs to train for.
+latent_dim = 64  # Latent dimensionality of the encoding space.
 num_samples = 10000  # Number of samples to train on.
 # Path to the data txt file on disk.
 data_path = "fra.txt"
